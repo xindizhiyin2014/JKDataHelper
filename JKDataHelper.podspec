d5846a09e0bd934029800f895a8e13f7a9bd7b4f
xindizhiyin2014 <929097264@qq.com>
2019-09-12 19:13:34 +0800

    update JKDataHelper

--- JKDataHelper.podspec
+++ JKDataHelper.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'JKDataHelper'
-  s.version          = '0.1.16.3'
+  s.version          = '0.1.17'
   s.summary          = 'this is a tool alalways to help developer to avoid bugs by the unstable APIs..'
 
 # This description is used to generate tags and improve search results.
--- JKDataHelper/Classes/NSArray+JKDataHelper.m
+++ JKDataHelper/Classes/NSArray+JKDataHelper.m
@@ -7,23 +7,9 @@
 //
 
 #import "NSArray+JKDataHelper.h"
-#import "JKDataHelperMacro.h"
-#import "NSObject+JK.h"
 
 @implementation NSArray (JKDataHelper)
-#ifdef JKDataHelperDebug
 
-+ (void)load{
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        Class targetClass = NSClassFromString(@"__NSArrayI");
-        [self JKswizzleMethod:@selector(objectAtIndex:) withMethod:@selector(JKsafeObjectAtIndex:) withClass:targetClass];
-        [self JKswizzleMethod:@selector(objectAtIndexedSubscript:) withMethod:@selector(JKsafeObjectAtIndexedSubscript:) withClass:targetClass];
-         [self JKswizzleMethod:@selector(initWithObjects:count:) withMethod:@selector(JKsafeInitWithObjects:count:) withClass:NSClassFromString(@"__NSPlaceholderArray")];
-    });
-}
-
-#endif
 
 -(id)jk_objectWithIndex:(NSInteger)index{
     
@@ -340,41 +326,6 @@
     CGRect rect = CGRectFromString(value);
     return rect;
 }
-
-#ifdef JKDataHelperDebug
-
-- (id)JKsafeObjectAtIndex:(NSInteger)index{
-    if (index >=0 && index < self.count) {
-        return [self JKsafeObjectAtIndex:index];
-    }else{
-        JKDataHelperLog(@"[__NSArrayI objectAtIndex:] index is greater than the array.count or the index is less than zero");
-        return nil;
-    }
-}
-
-- (id)JKsafeObjectAtIndexedSubscript:(NSInteger)index{
-    if (index >=0 && index < self.count) {
-        return [self JKsafeObjectAtIndex:index];
-    }else{
-        JKDataHelperLog(@"[__NSArrayI objectAtIndexedSubscript:] index is greater than the array.count or the index is less than zero");
-        return nil;
-    }
-}
-
-
-- (instancetype)JKsafeInitWithObjects:(int **)objects count:(NSInteger)count{
-    for (int i=0; i<count; i++) {
-        if (!objects[i]) {
-            JKDataHelperLog(@"%@", [NSString stringWithFormat:@"[__NSPlaceholderArray initWithObjects:count:]: attempt to insert nil to objects[%d]",i]);
-            return nil;
-        }
-    }
-    return [self JKsafeInitWithObjects:objects count:count];
-}
-
-#endif
-
-
 
 
 @end
